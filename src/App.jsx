806652e99905d548fe2d9fada0e6c19b40da9287
saiklangth <gao.sk@hotmail.com>
2025-05-16 16:12:08 +0700

    increase & decrease button

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,65 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 
 import Header from './components/Header'
 import ProductList from './components/ProductList'
 import CartSummary from './components/CartSummary'
 
 function App() {
-  const [products, setProducts] = useState([]) 
+  const [products, setProducts] = useState([])
   const [carts, setCarts] = useState([])
 
   // cart = {id, product, price, title, quantity}
 
   const fetchProducts = () => {
     fetch('http://localhost:8000/products')
-    .then(resp => resp.json())
-    .then(data => setProducts(data))
+      .then(resp => resp.json())
+      .then(data => setProducts(data))
   }
 
-  useEffect( () =>{
+  useEffect(() => {
     fetchProducts()
   }, [])
-
+  // ถ้า add สินค้าให้เพิ่มจำนวนแทน
   const addToCart = (id, title, price) => {
-    let newItem = { id: id, title: title, price: price, quantity: 1}
-    setCarts([...carts, newItem])
+    let idx = carts.findIndex(el => el.id === id) // not found = -1
+    let newItem
+    if (idx === -1) {
+      newItem = { id: id, title: title, price: price, quantity: 1 }
+      setCarts([...carts, newItem])
+    } else {
+      const cloneCart = [...carts]
+      cloneCart[idx].quantity += 1
+      setCarts(cloneCart)
+    }
+    // let newItem = { id: id, title: title, price: price, quantity: 1}
+    // setCarts([...carts, newItem])
   }
+
+  const decQuantity = (id) => {
+    // ลดจำนวน Quantity ของ item ใน cart
+    let idx = carts.findIndex(el => el.id === id) // not found = -1
+    const cloneCart = [...carts]
+    if(cloneCart[idx].quantity > 1) {
+      cloneCart[idx].quantity -= 1
+    } else {
+      cloneCart.splice(idx,1)
+    }
+    setCarts(cloneCart)
+  }
+
+  // const incQuantity = (id) => {
+  //   let idx = carts.findIndex(el => el.id === id) // not found = -1
+  //   const cloneCart = [...carts]
+  //   cloneCart[idx].quantity += 1
+  //   setCarts(cloneCart)
+  // }
 
   return (
     <div className='min-h-screen flex flex-col'>
-      <Header itemCount={carts.length}/>
+      <Header itemCount={carts.length} />
       <div className='flex flex-1'>
-        <ProductList products={products} addToCart={addToCart}/>
-        <CartSummary carts={carts}/>
+        <ProductList products={products} addToCart={addToCart} />
+        <CartSummary carts={carts} decQuantity={decQuantity} addToCart={addToCart}/>
       </div>
     </div>
   )
--- src/components/CartSummary.jsx
+++ src/components/CartSummary.jsx
@@ -2,16 +2,17 @@ import React from 'react'
 import CartItem from './CartItem'
 
 function CartSummary(props) {
-  const { carts } = props
+  const { carts, decQuantity, addToCart } = props
   return (
     <div className='w-1/3'>
       <h2 className='text-2xl py-2'>Cart Item :</h2>
+      {carts.length === 0 && <p>Empty cart</p>}
       <div className='flex flex-col gap-2'>
-        <CartItem />
-        <CartItem />
-        <CartItem />
+        {carts.map(el => (
+          <CartItem key={el.id} item={el} decQuantity={decQuantity} addToCart={addToCart} />
+        ))}
       </div>
-      <pre>{JSON.stringify(carts,null,2)}</pre>
+      {/* <pre>{JSON.stringify(carts,null,2)}</pre> */}
     </div>
   )
 }
